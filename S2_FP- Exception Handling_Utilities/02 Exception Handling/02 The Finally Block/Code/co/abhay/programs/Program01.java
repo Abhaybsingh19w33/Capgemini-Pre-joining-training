595429bb7d01e3a071d5edc2ab7539e5e829f08b
Abhay Singh <abhaybsingh19w33@gmail.com>
2022-04-27 12:38:03 +0530

    S2_FP- Exception Handling_Utilities : Exception Handling : The finally block

---
+++ S2_FP- Exception Handling_Utilities/02 Exception Handling/02 The Finally Block/Code/co/abhay/programs/Program01.java
@@ -0,0 +1,55 @@
+package co.abhay.programs;
+
+import java.util.*;
+import java.io.*;
+
+public class Program01
+{
+    public static void main( String args[]) {
+        
+        // String name;
+        // name = null;
+        // int len = name.length();
+
+        // System.out.println(name); // NUllPointerException
+
+        FileReader reader;
+        // compilatioon fails when no try catch block is found
+        reader = new FileReader("dummy.txt");
+
+        reader.close();
+        System.out.println("demo finished");
+
+        try {
+            int n, d, q;
+            // ArraylndexOutOfBoundsException
+            // NumberFormatException
+            n = Integer.parseInt (args [0]);
+            d = Integer.parseInt (args [1]);
+            q = n/d; // ArithmeticException
+            System.out.println("Quotient = " + q);
+        }
+        catch (NumberFormatException e) {
+            System.out.println("only works with integers");
+        }
+        catch (ArraylndexOutOfBoundsException e) {
+            System.out.println("two inputs required!");
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Second number cannot be zero");
+            // return; // to go to jre main function
+            // but finally block will be executed even using return
+
+            throw new RuntimeException("Custom throwing");
+            // even using throw finally block is still executed
+
+            System.exit(0); // in this case finally will not be executed // jvm quits immediately
+        }
+        catch(Exception e) {
+            System.out.println("Something went wrong.");
+        }
+        finally {
+            System.out.println("Inside Finally");
+        }
+    }
+}
